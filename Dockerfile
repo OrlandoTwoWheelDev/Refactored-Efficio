901e465a6903cf716376ddd0ba4271533cedb74e
Orlando Pacheco Jr <orlando.pacheco906@gmail.com>
2025-04-18 11:06:10 -0400

    Setup backend and frontend with Docker, added hot reloading

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# 1. Base image
+FROM node:23-slim
+
+# 2. Set working directory
+WORKDIR /app
+
+# 3. Copy files
+COPY package*.json ./
+COPY tsconfig*.json ./
+COPY vite.config.ts ./
+COPY . .
+
+# 4. Install deps
+RUN npm install
+
+# 5. Build the app
+RUN npm run build
+
+# 6. Expose port (if needed)
+EXPOSE 3000
+
+# 7. Start the app
+CMD ["npm", "run", "Start"]
--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,33 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import reactX from "eslint-plugin-react-x";
+import reactDom from "eslint-plugin-react-dom";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+export default tseslint.config({
+  ignores: ["eslint.config.js"],
+  extends: [
+    ...tseslint.configs.recommendedTypeChecked, // Or use strictTypeChecked for stricter rules
+    ...tseslint.configs.stylisticTypeChecked, // Optional for stylistic rules
+    "plugin:prettier/recommended"
+  ],
+  plugins: {
+    "react-x": reactX,
+    "react-dom": reactDom,
+    "react-hooks": reactHooks, // If you want to include react-hooks plugin rules
+    "react-refresh": reactRefresh, // If you're using React Fast Refresh
+  },
+  rules: {
+    // Enable React-specific rules for TypeScript
+    ...reactX.configs["recommended-typescript"].rules,
+    ...reactDom.configs.recommended.rules,
+    ...reactHooks.configs.recommended.rules, // React Hooks rules
+    ...reactRefresh.configs.recommended.rules, // React Refresh rules if needed
+  },
+  languageOptions: {
+    parserOptions: {
+      project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+      tsconfigRootDir: import.meta.dirname,
     },
   },
-)
+});
