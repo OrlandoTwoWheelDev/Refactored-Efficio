bc1123ff61ea6fe35b1ea95b73772b1c8c80a4ca
Orlando Pacheco Jr <orlando.pacheco906@gmail.com>
2025-04-18 07:53:35 -0400

    Rebirth of the Phoenix: Refactor and update readme

--- routes/index.cjs
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.use('/projects', require('./projectsRoutes.cjs'));
-router.use('/tasks', require('./tasksRoutes.cjs'));
-router.use('/teams', require('./teamsRoutes.cjs'));
-router.use('/users', require('./usersRoutes.cjs'));
-
-module.exports = router;
