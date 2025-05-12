10c039d4d0476f346bf1d44c98a6c33e98c8e367
Orlando Pacheco Jr <orlando.pacheco906@gmail.com>
2025-05-12 13:47:08 -0400

    more adjustments

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ const server = createServer(app);
 
 initSocket(server);
 
-app.use(cors());
+app.use(cors({origin: '*'}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../dist')));
 app.use(mainRouter);
